9423b192264659052c053cf48ada2ba448ba96d7
Luciano Romualdo <luciano.oliveira@lynxprocess.com>
2024-04-01 09:53:24 -0300

    Como fazer verificação de erros no body para que o Alura Books Server seja mais robusto e à prova de erros; Como garantir que os IDs buscados sejam existentes, para que o usuário saiba possíveis problemas com sua requisição.

--- controladores/livro.js
+++ controladores/livro.js
@@ -13,8 +13,17 @@ function getLivros(req, res) {
 function getLivro(req, res) {
     try {
         const id = req.params.id //aqui eu pego o parametro do id
-        const livro = getLivroPorId(id)
-        res.send(livro)        
+
+        if(id && Number(id)) { //se for um id e se o id for um numero...
+            //Number(2) -> 2 -> true
+            //Number("batata") -> NaN (Not a Number) -> false
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
+
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -24,9 +33,16 @@ function getLivro(req, res) {
 function postLivro(req, res) {
     try {
         const livroNovo = req.body
-        insereLivro(livroNovo)
-        res.status(201) //201 é o status de criado
-        res.send("Livro inserido com sucesso")
+
+        if(req.body.nome) { //verifico se o campo nome está preenchido
+            insereLivro(livroNovo)
+            res.status(201) //201 é o status de criado
+            res.send("Livro inserido com sucesso")
+        } else {
+            res.status(422)
+            res.send("O campo nome é obrigatório")
+        }
+
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -35,11 +51,16 @@ function postLivro(req, res) {
 
 function patchLivro(req, res) {
     try {
-        const id = req.params.id
-        const body = req.body
+        const id = req.params.id //primeiro e pego o id
 
-        modificaLivro(body, id)
-        res.send("Item modificado com sucesso")
+        if(id && Number(id)) { //depois valido se o mesmo é válido
+            const body = req.body
+            modificaLivro(body, id)
+            res.send("Item modificado com sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
 
     } catch (error) {
         res.status(500)
@@ -50,8 +71,15 @@ function patchLivro(req, res) {
 function deleteLivro(req, res) {
     try {
         const id = req.params.id
-        deletaLivroPorId(id)
-        res.send("Livro deletado com sucesso")
+
+        if(id && Number(id)) {
+            deletaLivroPorId(id)
+            res.send("Livro deletado com sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
+
     } catch (error) {
         res.status(500)
         res.send(error.message)
